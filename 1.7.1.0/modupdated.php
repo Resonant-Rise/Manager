b0c6b634307c4db886de17875d56d85c323ec88f
Ryan Carr <ryancarr10@gmail.com>
2016-02-07 21:09:19 -0500

    Added 1.7.10 files

---
+++ 1.7.1.0/modupdated.php
@@ -0,0 +1,29 @@
+<?php
+include('../db.php');
+
+$id = $_REQUEST['modid'];
+
+$query = "SELECT * FROM 170 where id = '$id'";
+$result = mysqli_query($con, $query);
+if (mysqli_num_rows($result) > 0) {
+	$query2 = "UPDATE 170 SET last_updated=UNIX_TIMESTAMP(now()) WHERE id = '$id'";
+	$result2 = mysqli_query($con, $query2) or die (mysqli_error($con));
+	while($row = mysqli_fetch_assoc($result)) {
+	echo $row['name'] . " updated! <br />";
+	echo "<a href='./index.php'>Back</a>";
+
+	exit;
+
+}
+
+} else {
+	while($row = mysqli_fetch_assoc($result)) {
+	echo $row['name'] . "could not be updated!";
+}
+
+}
+
+mysqli_close($con);
+
+	exit;
+?>
---
+++ 1.7.1.0/nomod.php
@@ -0,0 +1,23 @@
+<?php
+include('../db.php');
+
+$id = $_REQUEST['modid'];
+
+$query = "SELECT * FROM 170 where id = '$id'";
+$result = mysqli_query($con, $query);
+if (mysqli_num_rows($result) > 0) {
+	$query2 = "UPDATE 170 SET added='1' WHERE id = '$id'";
+	$result2 = mysqli_query($con, $query2) or die (mysqli_error($con));
+	while($row = mysqli_fetch_assoc($result)) {
+	echo $row['name'] . " marked as not included! <br />";
+	echo "<a href='./index.php'>Back</a>";
+
+}
+
+} else {
+	while($row = mysqli_fetch_assoc($result)) {
+	echo $row['name'] . "could not be updated!";
+}
+}
+mysqli_close($con);
+?>
