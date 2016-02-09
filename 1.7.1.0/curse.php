1a99868aa1cd379d38d293609c1961e73c81b444
Ryan Carr <ryancarr10@gmail.com>
2016-02-09 12:17:11 -0500

    test

--- 1.7.1.0/curse.php
+++ 1.7.1.0/curse.php
@@ -1,34 +1,26 @@
 <?php
-// create curl resource
-//    $ch = curl_init();
-//
-//    // set url
-//    curl_setopt($ch, CURLOPT_URL, "https://widget.mcf.li/mc-mods/minecraft/229218-extracells2.json");
-//
-//    //return the transfer as a string
-//    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-//    curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
-//
-//    // $output contains the output string
-//  $json = json_decode(curl_exec($ch), true);
-$url = 'https://widget.mcf.li/mc-mods/minecraft/229218-extracells2.json';
-$content = file_get_contents($url);
-$json = json_decode($content, true);
+$ch = curl_init();
 
-function isitempty($val){
-    if (trim($val) === ''){$val = "NULL";}
-    return $val;
-}
+    // set url
+    curl_setopt($ch, CURLOPT_URL, "bot.notenoughmods.com/1.8.9.json");
 
-echo $json['title'];
-echo '<br />';
-echo '<img src="' . $json['thumbnail'] . '" alt="Smiley face" height="57" width="57"><br />';
-echo $json['download']['url'];
-echo '<br />';
-echo $json['download']['version'];
+    //return the transfer as a string
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
 
+    // $output contains the output string
+  $json = json_decode(curl_exec($ch), true);
 
-
-$html = file_get_contents("http://minecraft.curseforge.com/projects/project-ores");
+foreach($json as $item) {
+    $link = $item['longurl'];
+    $link = preg_replace("/htt.{1,2}:\/\/(.+?[\.\-])*(\w{1,61}\.[a-zA-Z]{2,})\/.*/i", "$2", $link);
+    if ($link == 'curse.com') {
+        $url = $['longurl'];
+        $newurl = explode("http://www.curse.com", $url);
+        echo 'https://widget.mcf.li' . $newurl[1] . '.json <br />';
+    } elseif ($link == 'curseforge.com') {
+        $html = file_get_contents($item['longurl']);
 preg_match('%<li class="view-on-curse">\s+<a href="http:\/\/curse\.com\/project\/(?P<id>.*)">\s+View on Curse\.com\s+<\/a>\s+<\/li>%', $html, $matches);
-echo 'https://widget.mcf.li/project/' . $matches['id'] . '.json';
+echo 'https://widget.mcf.li/project/' . $matches['id'] . '.json <br />';
+    }
+}
