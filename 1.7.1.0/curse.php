410b928a055a4137f0b4e3446d50cc4cbd721221
Ryan Carr <ryancarr10@gmail.com>
2016-02-09 20:50:47 -0500

    test

--- 1.7.1.0/curse.php
+++ 1.7.1.0/curse.php
@@ -7,16 +7,22 @@ $result = mysqli_query($con, $query);
     //Sets date and time to EST. Currently stored as UTC
 	date_default_timezone_set('America/New_York');
     while($row = mysqli_fetch_assoc($result)) {
-
-        //Process for FQDN
-        $link = preg_replace("/htt.{1,2}:\/\/(.+?[\.\-])*(\w{1,61}\.[a-zA-Z]{2,})\/.*/i", "$2", $row['link']);
         //If its curse.com, process normally
-        if ($link == 'curse.com') {
             $newurl = explode("http://www.curse.com", $row['link']);
             $parsed = 'https://widget.mcf.li' . $newurl[1] . '.json';
-            $url = file_get_contents($parsed);
-            $cursej = json_decode($content, true);
-            echo $cursej['title'];
+            // create curl resource
+    $ch = curl_init();
+
+    // set url
+    curl_setopt($ch, CURLOPT_URL, "https://widget.mcf.li/mc-mods/minecraft/227049-iron-backpacks.json");
+
+    //return the transfer as a string
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+
+    // $output contains the output string
+  $json = json_decode(curl_exec($ch), true);
+        echo $json['title'];
 //            $ch1 = curl_init();
 //
 //        // set url
@@ -30,6 +36,7 @@ $result = mysqli_query($con, $query);
 //           $pjson = json_decode(curl_exec($ch1), true);
 //            echo $pjson['title'] . "<br />" . '<img src="' . $pjson['thumbnail'] . '" alt="' . $pjson['title'] . '"><br />' . $pjson['download']['url'] . "<br /><br />";
         }
+        echo $parsed . '<br />';
 
     //If its curseforge.com, process HTML
 //        if ($link == 'curseforge.com') {
