fa2f1af8451c24f0b343d2319d1062fb9fdace2f
Ryan Carr <ryancarr10@gmail.com>
2016-02-10 12:12:02 -0500

    More linking issues

--- header.php
+++ header.php
@@ -35,7 +35,7 @@ document.getElementById("indivload").innerHTML='<object type="text/html" data="1
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
 <!--        <li class="active"><a href="#" onclick="load_home()">1.7.10 <span class="sr-only">(current)</span></a></li>-->
-        <li class="active"><a href="http://mods.resonant-rise.com/1.7.1.0/index.php">1.7.10 <span class="sr-only">(current)</span></a></li>
+        <li class="active"><a href="http://mods.resonant-rise.com/1.7.10/index.php">1.7.10 <span class="sr-only">(current)</span></a></li>
         <li><a href="http://mods.resonant-rise.com/1.8.9/index.php">1.8.9</a></li>
         <?php if(is_minLevel(2)) { ?>
         <li class="dropdown">
