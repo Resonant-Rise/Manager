f7cee6f768526ae2151d23b9be4046f3d5f223b4
Ryan Carr <ryancarr10@gmail.com>
2016-02-10 12:02:51 -0500

    Added copy right

--- footer.php
+++ footer.php
@@ -1,5 +1,13 @@
+<?php function auto_copyright($year = 'auto'){
+   if(intval($year) == 'auto'){ $year = date('Y'); }
+    if(intval($year) == date('Y')){ echo intval($year); }
+    if(intval($year) < date('Y')){ echo intval($year) . ' - ' . date('Y'); }
+    if(intval($year) > date('Y')){ echo date('Y'); }
+ } ?>
+
 <footer>
-Hello
+    Resonant Rise Manager &copy; <?php autho_copyright('2015'); ?>
+Resonant Rise Manager is property of Resonant Rise. Resonant Rise is maintained by the Resonant Rise development team.
 </footer>
 </body>
 </html>
