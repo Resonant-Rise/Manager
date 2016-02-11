9f22762c0d1369cada28ce2074ee240bfcfa05d8
Ryan Carr <ryancarr10@gmail.com>
2016-02-11 03:00:22 -0500

    Real time search

--- header.php
+++ header.php
@@ -28,18 +28,9 @@ document.getElementById("indivload").innerHTML='<object type="text/html" data="1
 </script>
 -->
         <script type="application/javascript">
-        var $rows = $('#table tr');
-$('#search').keyup(function() {
-
-    var val = '^(?=.*\\b' + $.trim($(this).val()).split(/\s+/).join('\\b)(?=.*\\b') + ').*$',
-        reg = RegExp(val, 'i'),
-        text;
-
-    $rows.show().filter(function() {
-        text = $(this).text().replace(/\s+/g, ' ');
-        return !reg.test(text);
-    }).hide();
-});
+        $(document).ready( function () {
+    $('#table').DataTable();
+} );
         </script>
     </head>
     <body>
