4b3415e1aa346c3638f26565cc379681ffb529cf
Ryan Carr <ryancarr10@gmail.com>
2016-02-10 21:37:29 -0500

    Forgot these tables

--- 1.8.9/modupdated.php
+++ 1.8.9/modupdated.php
@@ -6,7 +6,7 @@ $id = $_REQUEST['modid'];
 $query = "SELECT * FROM 189 where id = '$id'";
 $result = mysqli_query($con, $query);
 if (mysqli_num_rows($result) > 0) {
-	$query2 = "UPDATE mods SET last_updated=UNIX_TIMESTAMP(now()) WHERE id = '$id'";
+	$query2 = "UPDATE 189 SET last_updated=UNIX_TIMESTAMP(now()) WHERE id = '$id'";
 	$result2 = mysqli_query($con, $query2) or die (mysqli_error($con));
 	while($row = mysqli_fetch_assoc($result)) {
 	echo $row['name'] . " updated! <br />";
