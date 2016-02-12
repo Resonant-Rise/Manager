d53e230e876ecedb97ef40f7c31f80d0476917b9
Ryan Carr <ryancarr10@gmail.com>
2016-02-11 21:00:14 -0500

    database fix

--- 1.7.10/modupdated.php
+++ 1.7.10/modupdated.php
@@ -1,14 +1,15 @@
 <?php
 include('../db.php');
 include('../login/includes/api.php');
+
 $user = userValue(null, "username");
 $id = $_REQUEST['modid'];
 $muser = $_REQUEST['user'];
 echo $muser;
-$query = "SELECT * FROM 1710 where id = '$id'";
+$query = "SELECT * FROM `1710` where id = '$id'";
 $result = mysqli_query($con, $query);
 if (mysqli_num_rows($result) > 0) {
-	$query2 = "UPDATE 1710 SET last_updated=UNIX_TIMESTAMP(now()), modified_by='$user' WHERE id = '$id'";
+	$query2 = "UPDATE `1710` SET last_updated=UNIX_TIMESTAMP(now()), modified_by='$user' WHERE id = '$id'";
 	$result2 = mysqli_query($con, $query2) or die (mysqli_error($con));
 	while($row = mysqli_fetch_assoc($result)) {
 	echo $row['name'] . " updated! <br />";
