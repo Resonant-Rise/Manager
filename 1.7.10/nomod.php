7461c2df2c0d3285a3a9784b729989ebe16ef4ab
Ryan Carr <ryancarr10@gmail.com>
2016-02-11 20:53:44 -0500

    Fixing user tracking

--- 1.7.10/nomod.php
+++ 1.7.10/nomod.php
@@ -1,13 +1,14 @@
 <?php
 include('../db.php');
-
+include('../login/includes/api.php');
+$user = userValue(null, "username");
 $id = $_REQUEST['modid'];
-$muser = $_REQUEST['muser'];
-
+$muser = $_REQUEST['user'];
+echo $muser;
 $query = "SELECT * FROM `1710` where id = '$id'";
 $result = mysqli_query($con, $query);
 if (mysqli_num_rows($result) > 0) {
-	$query2 = "UPDATE `1710` SET added='1', modified_by='$muser' WHERE id = '$id'";
+	$query2 = "UPDATE `1710` SET added='1', modified_by='$user' WHERE id = '$id'";
 	$result2 = mysqli_query($con, $query2) or die (mysqli_error($con));
 	while($row = mysqli_fetch_assoc($result)) {
 	echo $row['name'] . " marked as not included! <br />";
