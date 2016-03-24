2ce7f9b2102555c525a18f5b2a6d7a5715a87d0a
Ryan Carr <ryancarr10@gmail.com>
2016-03-24 14:25:03 -0400

    Fixing update

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
   <head>
    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
    <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css">
-<<<<<<< HEAD
 <!--
    <link rel="stylesheet" type="text/css" href="css/bootswatch.scss">
    <link rel="stylesheet" type="text/css" href="css/variables.scss">
@@ -17,10 +16,6 @@
         <article class="navbar-header">
          <h1>Hi everyone Welcome to the new site Tell us what you think</h1>
 
-
-=======
-   <!-- <link rel="stylesheet" type="text/css" href="css/bootswatch.scss">
-   <link rel="stylesheet" type="text/css" href="css/variables.scss"> -->
   </head>
    <body>
       <nav class="navbar navbar-default">
@@ -29,7 +24,6 @@
          </div>
        </div>
       </nav>
->>>>>>> Joseph
    </body>
     <footer>
       <script src="js/jquery-2.2.2.min.js"></script>
