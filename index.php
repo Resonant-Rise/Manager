c71439faa89df9e89988c1fed71633feb89127a6
Joseph Rymer <josephrymer983@gmail.com>
2016-03-24 00:42:28 -0500

    Work 01
    Added Referecne file tags and some HTML5

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+ ?>
+ <html>
+  <head>
+   <link rel="stylesheet" type="text/css" href="boostrap.css">
+   <link rel="stylesheet" type="text/css" href="boostrap-theme.css">
+   <link rel="stylesheet" type="text/css" href="bootswatch.css">
+   <link rel="stylesheet" type="text/css" href="variables.css">
+  </head>
+   <body>
+    <nav class="navbar navbar-default">
+      <article class="container-fluid">
+        <article class="navbar-header">
+
+
+   </body>
+    <footer>
+      <script src="js/jquery-2.2.2.min.js"></script>
+      <script src="js/boostrap.min.js"></script>
+    </footer>
+ </html>
