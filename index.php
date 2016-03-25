afa0aefe9b7b56f8dc0baca97431f62254833814
Joseph Rymer <josephrymer983@gmail.com>
2016-03-25 02:03:21 -0500

    Work 05
    index.php logo and intro added
    logo.png added
    stylesheet.css has many styles added imported from an old app

--- index.php
+++ index.php
@@ -10,16 +10,30 @@
    <link rel="stylesheet" type="text/css" href="css/variables.scss"> -->
   </head>
    <body>
-      <nav class="navbar navbar-inverse">
-       <div class="container-fluid">
-         <div class="navbar-header">
-
+      <nav class="navbar navbar-default navbar-fixed-top ">
+       <div id="nav-top" class="container-fluid">
+         <div class="navbar-inner">
+           <div class="navbar-header">
+             <a id="brand-image" class="navbar-brand" href="index.php">
+               <img alt="Brand" src="img/logo.jpg">
+             </a>
+          </div>
          </div>
        </div>
       </nav>
-      <ol id="navbar-breadcrumb"class="breadcrumb">
-        <li><a href="index.php">Home</a></li>
-      </ol>
+      <div class="container">
+        <div class="panel2 panel-primary">
+          <div class="panel-heading">
+            <h3 class="panel-title">Welcome!</h3>
+          </div>
+        </div>
+        <div class="jumbotron">
+          <h1>Oh, Hello there</h1>
+          <p>Resonant Rise Manager is an application built by hand and with love from the Resonant Rise Development Team.</p>
+          <p>With this application, it allows the developers to update the pack a little quicker.</p>
+          <p>Plus, there are a lot of features coming soon.</p>
+        </div>
+      </div>
    </body>
     <footer>
       <script src="js/jquery-2.2.2.min.js"></script>
