87dfd4957c48c4825138a70e3dbd3d44a0e6ca71
Joseph Rymer <josephrymer983@gmail.com>
2016-03-24 20:42:17 -0500

    Work 04
    Added stylesheet.css

--- index.php
+++ index.php
@@ -5,16 +5,21 @@
   <head>
    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
    <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css">
+   <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
    <!-- <link rel="stylesheet" type="text/css" href="css/bootswatch.scss">
    <link rel="stylesheet" type="text/css" href="css/variables.scss"> -->
   </head>
    <body>
-      <nav class="navbar navbar-default">
+      <nav class="navbar navbar-inverse">
        <div class="container-fluid">
          <div class="navbar-header">
+
          </div>
        </div>
       </nav>
+      <ol id="navbar-breadcrumb"class="breadcrumb">
+        <li><a href="index.php">Home</a></li>
+      </ol>
    </body>
     <footer>
       <script src="js/jquery-2.2.2.min.js"></script>
