3cc1c6856e0605db459d9c7db9ad59a76321bebb
Ryan Carr <ryancarr10@gmail.com>
2016-07-08 20:48:01 -0400

    Added database functionality

--- 1.10/update.php
+++ 1.10/update.php
@@ -1,7 +1,9 @@
 <html>
+
 <head>
-<!--<META http-equiv="refresh" content="1;URL=/1.8.9/index.php">-->
+    <!--<META http-equiv="refresh" content="1;URL=/1.8.9/index.php">-->
 </head>
+
 </html>
 <?php
 include('../db.php');
@@ -9,30 +11,46 @@ function isitempty($val){
     if (trim($val) === ''){$val = "NULL";}
     return $val;
 }
-$query = "SELECT id,name,mcf_link FROM `1102`";
+$query = "SELECT id,name,mcf_link FROM `1102` where id=1192";
 $result = mysqli_query($con, $query);
 
 if (mysqli_num_rows($result) > 0) {
 	while($row = mysqli_fetch_assoc($result)) {
 		$link = $row['mcf_link'];
 		$id = $row['id'];
-		$modname = $row['name'];
         
         $file = file_get_contents($link);
         $json = json_decode($file, true);
+        $authorfinal = '';
 
-foreach ($json as $json1)
-{
-    
 
-        echo $json1["url"] . "<br />";
-        echo $json1["authors"] . "<br />";
-        echo $json1["updated_at"] . "<br />";
-        echo $json1["project_url"] . "<br />";
-        echo $json1["release_type"] . "<br />";
-        echo $json1["license"] . "<br />";
-        echo $json1["download"]["url"] . "<br />";
-}
+    foreach ($json['authors'] as $author)
+    {
+        if(!empty($authorfinal)) {
+            $authorfinal .=';';
+        }
+        $authorfinal .= $author;
+    }
+        $update = isitempty($json['updated_at']);
+        $project = isitempty($json['project_url']);
+        $release = isitempty($json['release_type']);
+        $license = isitempty($json['license']);
+        $download = isitempty($json['download']['url']);
+        
+        $query = "INSERT INTO 1102 (release,author,repo,license,download_link) VALUES ($release,$authorfinal,$project,$license,$download)
+        ON DUPLICATE KEY UPDATE
+        release = VALUES(release),
+        download_link = VALUES(download_link),
+        repo = VALUES(repo),
+        license = VALUES(license),
+        author = VALUES(author)
+        ";
+        
+        
+        $sql = mysqli_real_escape_string($con, $query);
+
+        mysqli_query($con, $sql) or die (mysqli_error($con));
+
     
 
     }
