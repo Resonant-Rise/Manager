c8ebcfc4d7dc2a2ae1ae841b0fac7eeef651bbc5
Ryan Carr <ryancarr10@gmail.com>
2016-07-09 20:47:07 -0400

    Attempt at fixing database insertion error

--- 1.10/update.php
+++ 1.10/update.php
@@ -31,25 +31,26 @@ if (mysqli_num_rows($result) > 0) {
         }
         $authorfinal .= $author;
     }
-        $update = isitempty($json['updated_at']);
-        $project = isitempty($json['project_url']);
-        $release = isitempty($json['release_type']);
-        $license = isitempty($json['license']);
-        $download = isitempty($json['download']['url']);
+        $update = mysqli_real_escape_string($con, isitempty($json['updated_at']));
+        $project = mysqli_real_escape_string($con, isitempty($json['project_url']));
+        $release = mysqli_real_escape_string($con, isitempty($json['release_type']));
+        $license = mysqli_real_escape_string($con, isitempty($json['license']));
+        $download = mysqli_real_escape_string($con, isitempty($json['download']['url']));
         
-        $query = "INSERT INTO 1102 (release,author,repo,license,download_link) VALUES ($release,$authorfinal,$project,$license,$download)
-        ON DUPLICATE KEY UPDATE
+        $query = "INSERT INTO `1102` (release,author,repo,license,download_link) VALUES ('".$release."','".$authorfinal."','".$project."','".$license."','".$download."')
+        ON DUPLICATE KEY UPDATE 
         release = VALUES(release),
         download_link = VALUES(download_link),
         repo = VALUES(repo),
         license = VALUES(license),
-        author = VALUES(author)
+        author = VALUES(author) where id=$id
         ";
         
         
-        $sql = mysqli_real_escape_string($con, $query);
+//        $sql = mysqli_real_escape_string($con, $query);
+//        echo var_dump($query);
 
-        mysqli_query($con, $sql) or die (mysqli_error($con));
+        mysqli_query($con, $query) or die (mysqli_error($con));
 
     
 
